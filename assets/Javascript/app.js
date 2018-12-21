be54cf8415fb200f6bf01794390c352d8413b42b
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-12-21 13:30:59 -0700

    About Me Update

---
+++ assets/Javascript/app.js
@@ -0,0 +1,5 @@
+<form action="mailto:seneca2.cherry8@gmail.com" method="post" enctype="text/plain" >
+FirstName:<input type="text" name="FirstName">
+Email:<input type="text" name="Email">
+<input type="submit" name="submit" value="Submit">
+</form>
